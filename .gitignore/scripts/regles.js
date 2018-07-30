8413f480fca2f32a962f23fc9fed1c55c0740676
CHVRLY <smz@avgservers.fr>
2018-07-30 13:18:37 +0200

    Create regles.js

---
+++ .gitignore/scripts/regles.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+const clientDiscord = new Discord.Client();
+const config = require('./config.json');
+const PREFIX = "!c";
+
+clientDiscord.on('message', message => {
+    if(message.content.startsWith(PREFIX + " regles")) {
+            const embed = new Discord.RichEmbed();
+            embed.setTitle(' ')
+            .setAuthor('Réglement du discord:', clientDiscord.user.avatarURL)
+            .setColor(3447003)
+            .setDescription(
+`
+**Les règles ci-dessous sont à respecter obligatoirement :**
+
+**1.** Soyez respecteux avec les autres membres du discord.
+
+**2.** Merci de ne dévoiller aucune informations personnelles afin de respecter la vie privée de tous.
+
+**3.** Il est formellement interdit de poster des images choquantes/pornographiques dans les salons publics, des salons privés sont prévus à cet effet.
+
+**4.** Le spam est formellement interdit.
+
+`)
+            
+            message.channel.send({embed});
+        }
+});
+
+clientDiscord.login("process.env.TOKEN");
