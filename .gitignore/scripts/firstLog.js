8426719dac296479a6a6ea2bdb2482a8a9d2d328
CHVRLY <smz@avgservers.fr>
2018-07-30 14:19:55 +0200

    Create firstLog.js

---
+++ .gitignore/scripts/firstLog.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+const clientDiscord = new Discord.Client();
+const config = require('./config.json');
+const PREFIX = "!c";
+
+clientDiscord.on("guildMemberAdd", member => {
+    let role = member.guild.roles.find("name", "Membre");
+    member.guild.channels.find("name", "📢-accueil").send(` **${member.user.username}** viens de rejoindre le discord! :tada:`)
+    member.addRole(role)
+})
+
+clientDiscord.login(process.env.TOKEN);
