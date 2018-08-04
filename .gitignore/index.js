06067f8dd6299388742f3f548b2e3e90af179a48
CHVRLY <smz@avgservers.fr>
2018-08-04 12:11:39 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,8 +1,8 @@
 // DEPENDANCES
 const Discord = require('discord.js');
-const weather = require('weather-js');
 const config = require('./scripts/config.json');
 const scripts = require('./scripts/scripts.js');
+const weather = require('weather-js');
 require('events').EventEmitter.prototype._maxListeners = 1000;
 
 // VARIABLES
