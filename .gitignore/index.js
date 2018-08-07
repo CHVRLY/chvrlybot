9bdd05fa71ba1808ca893ed511a72356b08c6005
CHVRLY <smz@avgservers.fr>
2018-08-07 16:01:10 +0200

    Removing help & info commands for optimisation

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -5,6 +5,7 @@ const scripts = require('./scripts/scripts.js');
 const weather = require('weather-js');
 require('events').EventEmitter.prototype._maxListeners = 1000;
 
+
 // VARIABLES
 const clientDiscord = new Discord.Client();
 const PREFIX = "!c";
@@ -16,68 +17,5 @@ clientDiscord.on('ready', () => {
     clientDiscord.user.setActivity('!c help');
 });
 
-// !c help
-
-clientDiscord.on('message', message => {
-    if(message.content.startsWith(PREFIX + " help")) {
-            const embed = new Discord.RichEmbed();
-            embed.setTitle(' ')
-            .setAuthor('Chez Chvrly', clientDiscord.user.avatarURL)
-            .setColor(3447003)
-            .setDescription(
-`
-
-**>** La liste des commande viens de t'étre envoyée en MP !
-
-`)
-            
-            message.channel.send({embed});
-            message.author.createDM().then(channel => {
-                const embed = new Discord.RichEmbed();
-                embed.setTitle('Voici la liste des commandes :')
-                .setAuthor(' ', clientDiscord.user.avatarURL)
-                .setColor(3447003)
-                .setDescription(
-`
-
-__Commandes Principales :__
-
-**!c regles** -> Affiche le réglement.
-**!c météo [ville]** -> Affiche la météo en direct.
-**!c sondage <question>** -> Poste un sondage.
-**!c mas** -> Lance la machine à sous.
-
-__Commandes Secondaires :__
-
-**!c heure** -> Affiche l'heure.
-**!c info** -> Affiche les informations du bot.
-**!c clear** -> Nettoie le salon (admin).
-
-`)
-                
-                channel.send({embed});
-            })
-        }
-});
-
-// !c info
-
-clientDiscord.on('message', message => {
-    if(message.content.startsWith(PREFIX + " info")) {
-            const embed = new Discord.RichEmbed();
-            embed.setTitle('Fiche technique:')
-            .setAuthor('Informations de ChvrlyBot', clientDiscord.user.avatarURL)
-            .setColor(3447003)
-            .setDescription(
-`
-
-**> Version:** Alpha (v0.001)
-**> Auteur:** CHVRLY
-
-`)
-            
-            message.channel.send({embed});
-        }
-});
 
 clientDiscord.login(process.env.TOKEN);
