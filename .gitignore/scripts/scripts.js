5d7ea9d1324e2cd57b948279bb0c709e931063ae
CHVRLY <smz@avgservers.fr>
2018-08-07 14:33:39 +0200

    Calling slot.js

--- .gitignore/scripts/scripts.js
+++ .gitignore/scripts/scripts.js
@@ -4,3 +4,4 @@ const firstLog = require("./firstLog.js");
 const clear = require("./clear.js");
 const weather = require("./weather.js");
 const poll = require("./poll.js");
+const slot = require("./slot.js");
