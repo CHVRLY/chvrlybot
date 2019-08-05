a75b9145ef9875ec13f2c3921b2f5fccc9bd47fe
CHVRLY <smoezydesign@yahoo.com>
2019-08-05 21:48:11 +0200

    Create securite.js

---
+++ .gitignore/scripts/securite.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+const clientDiscord = new Discord.Client();
+const config = require('./config.json');
+const PREFIX = "!c";
+
+clientDiscord.on('message', function(message) {
+    if (message.content.startsWith(PREFIX + " sécurité")) {
+
+        if (!message.member.hasPermission("MANAGE_MESSAGES")) {
+            message.reply("**Tu es qui pour demander l'aide de la sécurité toi ?!**");
+        }
+
+        if (message.member.hasPermission("MANAGE_MESSAGES")) {
+            message.channel.send("**Je vous ordonne de laisser Charly tranquille! Ne m'obligez pas à faire usage de la force ! :cop:**")
+                
+        }
+    }
+});
+
+clientDiscord.login(process.env.TOKEN)
