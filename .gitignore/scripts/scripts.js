06a9606ec39f20d1dde92a5dbb3abb8bc3777f77
CHVRLY <smoezydesign@yahoo.com>
2020-08-22 04:30:07 +0200

    Update scripts.js

--- .gitignore/scripts/scripts.js
+++ .gitignore/scripts/scripts.js
@@ -1,5 +1,4 @@
 const heure = require("./heure.js");
-const regles = require("./regles.js");
 const firstLog = require("./firstLog.js");
 const clear = require("./clear.js");
 const weather = require("./weather.js");
