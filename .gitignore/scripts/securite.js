6cfc84978a2dda47ee7bf7c554d487be92fcee83
CHVRLY <smoezydesign@yahoo.com>
2020-08-22 20:49:03 +0200

    Update securite.js

--- .gitignore/scripts/securite.js
+++ .gitignore/scripts/securite.js
@@ -7,11 +7,11 @@ clientDiscord.on('message', function(message) {
     if (message.content.startsWith(PREFIX + "sécurité")) {
 
         if (!message.member.hasPermission("MANAGE_MESSAGES")) {
-            message.reply("**Tu es qui pour demander l'aide de la sécurité toi ?! :cop:**");
+            message.reply(" :man_with_probing_cane_tone1: ");
         }
 
         if (message.member.hasPermission("MANAGE_MESSAGES")) {
-            message.channel.send("**Je vous ordonne de laisser Charly tranquille! Ne m'obligez pas à faire usage de la force! :cop:**")
+            message.channel.send("**Je vous ordonne de laisser Charly tranquille ! Ne m'obligez pas à faire usage de la force !**")
                 
         }
     }
