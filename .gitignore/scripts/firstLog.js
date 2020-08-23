7c3517466439d85e377f966fde367c712efb9e56
CHVRLY <smoezydesign@yahoo.com>
2020-08-23 20:40:49 +0200

    Update firstLog.js

--- .gitignore/scripts/firstLog.js
+++ .gitignore/scripts/firstLog.js
@@ -4,12 +4,13 @@ const config = require('./config.json');
 
 clientDiscord.on("guildMemberAdd", member => {
     //let role = member.guild.roles.find("name", "👥 La plèbe");
-    member.guild.channels.find("id", "721774962641207338").send(` Accueillons **${member.user.name}** qui vient de rejoindre le discord! :Winnie: `)
+    //member.guild.channels.find("id", "721774962641207338").send(` Accueillons **${member.user.username}** qui vient de rejoindre le discord! :Winnie: `)
+    member.guild.channels.find("id", "721774962641207338").send(` Accueillons **${user}** qui vient de rejoindre le discord! :Winnie: `)
     //member.addRole(role)
 });
 
 clientDiscord.on("guildMemberRemove", member => {
-    member.guild.channels.find("id", "721774962641207338").send(` **${member.user.name}** vient de quitter le discord... :Winnie: `)
+    member.guild.channels.find("id", "721774962641207338").send(` **${user}** vient de quitter le discord... :Winnie: `)
 })
 
 clientDiscord.login(process.env.TOKEN);
